b8eea738acafe8063ec0f7868e0a7211b71a5c34
Davis <davisnguyen98@gmail.com>
2019-07-22 16:40:23 -0500

    Updates

---
+++ precache-manifest.f27202fe4ebdb14f5fe7ff36d150f943.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3898a5fd3fd40dce9445",
+    "url": "/todoapp/static/js/runtime~main.00cdf33a.js"
+  },
+  {
+    "revision": "a6f8ca1045952e104e6d",
+    "url": "/todoapp/static/js/main.28cd00f7.chunk.js"
+  },
+  {
+    "revision": "06a4bde0bcec44fb3fb8",
+    "url": "/todoapp/static/js/2.7859426b.chunk.js"
+  },
+  {
+    "revision": "6725d6623def1cb35ed06fc0b6e69206",
+    "url": "/todoapp/index.html"
+  }
+];
